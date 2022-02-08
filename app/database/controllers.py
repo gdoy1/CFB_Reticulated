f293ccc30ffcf2c1c5fedd06e492a28b49053c6f
rklocke <rebeccakatelocke@gmail.com>
2022-02-08 13:13:02 +0000

    Added function to calculate max quantity, updated controllers to return this value, updated html to display name of max quantity item and updatd  progress bar to set value

--- app/database/controllers.py
+++ app/database/controllers.py
@@ -8,6 +8,7 @@ DESCRIPTION:   Contains the Database class that contains all the methods used fo
 """
 
 from sqlalchemy.sql import func
+from sqlalchemy.sql import desc
 from flask import Blueprint
 
 from app import db
@@ -32,3 +33,13 @@ class Database:
     def get_n_data_for_PCT(self, pct, n):
         """Return all the data for a given PCT."""
         return db.session.query(PrescribingData).filter(PrescribingData.PCT == pct).limit(n).all()
+
+    def get_max_quantity(self):
+        """Find item with the maximum summed quantity"""
+        return db.session.query(PrescribingData.BNF_name, func.sum(PrescribingData.quantity)).group_by(PrescribingData.BNF_name).order_by(desc(func.sum(PrescribingData.quantity))).limit(1).all()
+
+    def find_max_quantity_percentage(self):
+        """Get the item with the maximum quantity as a percentage of all prescription"""
+        max_quantity = db.session.query(func.sum(PrescribingData.quantity)).group_by(PrescribingData.BNF_name).order_by(desc(func.sum(PrescribingData.quantity))).limit(1).all()[0][0]
+        all_quantities = db.session.query(func.sum(PrescribingData.quantity)).all()
+        return float(int((max_quantity) / int(all_quantities)) * 100)
